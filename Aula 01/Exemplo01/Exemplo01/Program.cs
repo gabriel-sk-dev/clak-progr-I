636863a2763732773273e67ad22ce7816e68c01e
gskohlrausch <gabriel@society.com.br>
2016-08-11 18:39:27 -0300

    aula 01 e pre aula 02

---
+++ Aula 01/Exemplo01/Exemplo01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world !!!!!");
+
+            Console.Write("Digite seu nome:");
+            string nomeDoUsuario = Console.ReadLine();
+
+            Console.WriteLine();
+            
+            Console.Write("Seja bem vindo " + nomeDoUsuario);
+
+
+            // Para o programa ficar executando
+            Console.ReadLine();            
+        }
+    }
+}
---
+++ Aula 01/Exemplo02/ClakTaxi/ClakTaxi/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClakTaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string leituraDoTeclado = "";
+
+            Console.Write("Informe a kilometragem inicial:");
+            leituraDoTeclado =  Console.ReadLine();
+            int kmInicial = Convert.ToInt32(leituraDoTeclado);
+
+            Console.Write("Informe a kilometragem final:");
+            leituraDoTeclado = Console.ReadLine();
+            int kmFinal = Convert.ToInt32(leituraDoTeclado);
+
+            decimal valorPorKilometro = 4.28m;
+
+            decimal valorCorrida = (kmFinal - kmInicial) * valorPorKilometro;
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("O valor a ser pago é de R$ " + valorCorrida);
+
+            // Para não fechar o programa
+            Console.WriteLine();
+            Console.WriteLine("Pressione qualquer tecla para fechar .....");
+            Console.ReadKey();
+        }
+    }
+}
