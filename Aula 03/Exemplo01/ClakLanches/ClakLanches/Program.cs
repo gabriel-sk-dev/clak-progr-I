e3f43d924d86e2f0ca8db567dc6ef98c6e02b80a
gskohlrausch <gabriel@society.com.br>
2016-08-18 19:44:49 -0300

    aula03 - exercicio 01 - resolvido

--- Aula 03/Exemplo01/ClakLanches/ClakLanches/Program.cs
+++ Aula 03/Exemplo01/ClakLanches/ClakLanches/Program.cs
@@ -9,7 +9,39 @@ namespace ClakLanches
     class Program
     {
         static void Main(string[] args)
-        {
+        {            
+            bool erro = true;
+            while (erro == true)
+            {
+                // Cabeçalho
+                Console.WriteLine("********************");
+                Console.WriteLine("****CLAK LANCHES****");
+                Console.WriteLine("********************");
+
+                Console.Write("Informe o nome do usuário:");
+                string nome = Console.ReadLine();
+                Console.Write("Informe a senha:");
+                string senha = Console.ReadLine();
+
+                if (nome == "joao" && senha == "123456")
+                {
+                    Console.WriteLine("Seja bem vindo Joao");
+                    erro = false;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Dados de login inválidos! Pressione qualquer tecla para tentar novamente ....");
+                    Console.ReadKey();
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Clear();
+                }
+            }
+            
+            // Final do programa
+            Console.WriteLine();
+            Console.Write("Pressione uma tecla para continuar ....");
+            Console.ReadKey();
         }
     }
 }
