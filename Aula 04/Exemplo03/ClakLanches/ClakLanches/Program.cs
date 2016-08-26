8306def6db1765c8143d9d38e799f17c0e36c6ad
gskohlrausch <gabriel@society.com.br>
2016-08-25 21:41:09 -0300

    aula 04 - exemplo 03 - inicio do problema

--- Aula 04/Exemplo03/ClakLanches/ClakLanches/Program.cs
+++ Aula 04/Exemplo03/ClakLanches/ClakLanches/Program.cs
@@ -8,10 +8,23 @@ namespace ClakLanches
 {
     class Program
     {
+        // SIMULACAO DO BANCO DE DADOS
+        static string[] codigos = new string[]
+            { "001", "002", "003" };
+        static string[] nomes = new string[]
+            { "coca cola", "sanduiche", "imigracao 1980" };
+        static string[] descricoes = new string[]
+            { "refrigerante lata", "Pao com atum", "Chopp weiss 500ml" };
+        static decimal[] precos = new decimal[]
+            { 4.5m, 10m, 16.20m };
+        static int[] quantidades = new int[]
+            { 20, 5, 10 };
+
+        // pegar cor padrão da letra
+        static ConsoleColor corPadrao = Console.ForegroundColor;
+
         static void Main(string[] args)
         {
-            // pegar cor padrão da letra
-            ConsoleColor corPadrao = Console.ForegroundColor;
 
             // Garantir que o programa fique rodando em loop
             bool possoSair = false;
@@ -28,7 +41,7 @@ namespace ClakLanches
                 Console.WriteLine();
                 Console.Write("O que deseja fazer? ");
                 ConsoleKeyInfo opcaoLida = Console.ReadKey();
-                              
+
                 // Tratar a tecla lida para descobrir o menu
                 switch (opcaoLida.Key)
                 {
@@ -36,6 +49,9 @@ namespace ClakLanches
                         Console.Clear();
                         // Se for 1 então cadastro produtos
                         Console.WriteLine("-------- CADASTRO DE PRODUTOS ---------");
+
+                        ExecutarRotinaDeCadastro();
+
                         Console.Write("Pressione qualquer tecla para continuar");
                         Console.ReadKey();
                         break;
@@ -43,6 +59,9 @@ namespace ClakLanches
                         Console.Clear();
                         // Se for 2 então venda de produtos
                         Console.WriteLine("-------- VENDA DE PRODUTOS ---------");
+
+                        ExecutarRotinaDeVendaDeProdutos();
+
                         Console.Write("Pressione qualquer tecla para continuar");
                         Console.ReadKey();
                         break;
@@ -58,8 +77,102 @@ namespace ClakLanches
                         Console.ReadKey();
                         Console.Clear();
                         break;
-                }                                
+                }
             }
+
+        }
+
+        public static void ExecutarRotinaDeVendaDeProdutos()
+        {
+            // Leitura do codigo
+            Console.Write("Digite o codigo do produto: ");
+            string codigoLido = Console.ReadLine();
+
+            int posicaoProduto = -1;
+
+            // Localizar o codigo dentro do vetor de codigos
+            for (int i = 0; i < codigos.Length; i++)
+            {
+                if (codigoLido == codigos[i])
+                {
+                    // Guardar a posição do produto no vetor
+                    posicaoProduto = i;
+                }
+            }
+
+            // Se localizei o produto exibir dados
+            if (posicaoProduto >= 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine(nomes[posicaoProduto]);
+                Console.WriteLine(descricoes[posicaoProduto]);
+                Console.WriteLine("R$ " + precos[posicaoProduto]);
+
+                // Perguntar quantas unidades deseja
+                Console.Write("Digite a quantidade: ");
+                int quantidadeDesejada = Convert.ToInt32(Console.ReadLine());
+
+                // Verificar se tenho estoque suficiente
+                int quantidadeEmEstoque = quantidades[posicaoProduto];
+                if (quantidadeDesejada > quantidadeEmEstoque)
+                {
+                    // Nao tenho mais estoque
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Não existe quantidade suficiente para venda!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    // Informar o valor total da compra
+                    decimal valorTotal = quantidadeDesejada * precos[posicaoProduto];
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Sua compra deu R$ {0}", valorTotal);
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                    // Diminuir o estoque
+                    quantidades[posicaoProduto] = quantidades[posicaoProduto] - quantidadeDesejada;
+                }
+            }
+        }
+
+        public static void ExecutarRotinaDeCadastro()
+        {
+            //Perguntar o codigo para ser criado
+            Console.Write("Digite o codigo do novo produto: ");
+            string codigoLido = Console.ReadLine();
+
+            int posicaoProduto = -1;
+
+            // Verificar se ja existe algum produto com o codigo
+            for (int i = 0; i < codigos.Length; i++)
+            {
+                if (codigoLido == codigos[i])
+                {
+                    posicaoProduto = i;
+                }
+            }
+
+            if (posicaoProduto >= 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Código já exsite!");
+                Console.ForegroundColor = corPadrao;
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+            else
+            {
+                // Perguntar o nome do produto, descricao, preco e quantidade                
+                Console.Write("Digite o nome do produto: ");
+                string nome = Console.ReadLine();
+
+                // Acrescentar nos vetores
+
+            }
+
+
+
 
         }
     }
